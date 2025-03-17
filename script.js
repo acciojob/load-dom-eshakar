b689653528883dcd07dbff3568cde184f9e66616
eshakar <102459513+eshakar@users.noreply.github.com>
2025-03-17 22:10:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
 //your JS code here. If required.
-document.addEventListener('DOMContentLoaded', function() {
-  // Create a new text node with the message
-  const textNode = document.createTextNode('DOM load success');
-  
-  // Append the text node directly to the body
-  document.body.appendChild(textNode);
-});
